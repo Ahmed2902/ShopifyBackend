970756706636ef8f4f5d0f9b19551b4718bc1ebe
Ahmed Abdelrahman <ahmedabdelrahma@gmail.com>
2026-08-23 08:45:30 +0300

    refactor(stores): remove single-use mapping helper [skip ci]

--- src/modules/stores/store.utils.ts
+++
@@ -1,6 +0,0 @@
-export function withMembershipRole<
-  T extends { memberships: Array<{ role: string }> },
->(store: T): Omit<T, 'memberships'> & { role: string | undefined } {
-  const { memberships, ...rest } = store;
-  return { ...rest, role: memberships[0]?.role };
-}
