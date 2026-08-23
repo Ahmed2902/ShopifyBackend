1f8c7d3a42c126b7f95c77a30202928dd555ef4d
Ahmed Abdelrahman <ahmedabdelrahma@gmail.com>
2026-08-23 15:24:38 +0300

    refactor: restore store controller layer

---
+++ src/modules/stores/store.controller.ts
@@ -0,0 +1,16 @@
+import type { Request, Response } from 'express';
+import type { StoreService } from './store.service.js';
+
+export class StoreController {
+  constructor(private readonly service: StoreService) {}
+
+  list = async (req: Request, res: Response) => {
+    const stores = await this.service.listForUser(req.context.userId!);
+    res.status(200).json({ stores });
+  };
+
+  getById = async (req: Request, res: Response) => {
+    const store = await this.service.getById(req.context.storeId!);
+    res.status(200).json({ store: { ...store, role: req.context.role } });
+  };
+}
