8f2ae601222800806c733d14ac89bb2891c8bb19
Ahmed Abdelrahman <ahmedabdelrahma@gmail.com>
2026-08-23 15:27:10 +0300

    refactor: restore express auth type import

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,4 +1,4 @@
-import type { StoreAccessClaim, StoreRoleClaim } from '../modules/auth/auth.utils.js';
+import type { StoreAccessClaim, StoreRoleClaim } from './auth.js';
 
 declare module 'express-serve-static-core' {
   interface Request {
