d73aece9726241a4d42488df8a048f2f9e69ef74
Ahmed Abdelrahman <ahmedabdelrahma@gmail.com>
2026-08-23 04:58:18 +0300

    feat(meta): compose catalog and insights services

--- src/modules/meta/meta.module.ts
+++ src/modules/meta/meta.module.ts
@@ -2,6 +2,10 @@ import { IntegrationRepository } from '../integrations/integration.repository.js
 import { IntegrationService } from '../integrations/integration.service.js';
 import { MetaAdsRepository } from './ads/meta-ads.repository.js';
 import { MetaAdsService } from './ads/meta-ads.service.js';
+import { MetaCatalogRepository } from './catalog/meta-catalog.repository.js';
+import { MetaCatalogService } from './catalog/meta-catalog.service.js';
+import { MetaInsightsRepository } from './insights/meta-insights.repository.js';
+import { MetaInsightsService } from './insights/meta-insights.service.js';
 import { MetaRepository } from './meta.repository.js';
 import { MetaService } from './meta.service.js';
 import { MetaApiService } from './shared/meta-api.service.js';
@@ -12,6 +16,10 @@ const metaApiService = new MetaApiService(metaRepository);
 const metaAuthService = new MetaAuthService(metaRepository, metaApiService);
 const metaAdsRepository = new MetaAdsRepository();
 const metaAdsService = new MetaAdsService(metaAdsRepository, metaApiService);
+const metaCatalogRepository = new MetaCatalogRepository();
+const metaCatalogService = new MetaCatalogService(metaCatalogRepository, metaApiService);
+const metaInsightsRepository = new MetaInsightsRepository();
+const metaInsightsService = new MetaInsightsService(metaInsightsRepository, metaApiService);
 const integrationService = new IntegrationService(new IntegrationRepository());
 
 export const metaService = new MetaService(
@@ -21,4 +29,8 @@ export const metaService = new MetaService(
   metaAdsService,
   metaAdsRepository,
   integrationService,
+  metaCatalogService,
+  metaCatalogRepository,
+  metaInsightsService,
+  metaInsightsRepository,
 );
