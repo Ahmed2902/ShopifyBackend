84d3651d894aa36c8de60393d437fd76e0d6f659
Ahmed Abdelrahman <ahmedabdelrahma@gmail.com>
2026-09-06 16:10:30 +0300

    feat(pixel): migrate cart view funnel metrics

---
+++ prisma/migrations/20260906000100_pixel_cart_view_funnel/migration.sql
@@ -0,0 +1,8 @@
+ALTER TYPE "StorefrontEventName" ADD VALUE 'CART_VIEW';
+
+ALTER TABLE "StorefrontSession"
+  ADD COLUMN "cartViewCount" INTEGER NOT NULL DEFAULT 0;
+
+ALTER TABLE "StorefrontBehaviorDaily"
+  ADD COLUMN "cartViewCount" INTEGER NOT NULL DEFAULT 0,
+  ADD COLUMN "cartViewSessionCount" INTEGER NOT NULL DEFAULT 0;
