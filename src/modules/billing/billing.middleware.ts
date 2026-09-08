139916112f2b68b0360a138e3ead7bf6046bc51e
Ahmed Abdelrahman <ahmedabdelrahma@gmail.com>
2026-09-08 20:34:14 +0300

    feat: add billing entitlement middleware

---
+++ src/modules/billing/billing.middleware.ts
@@ -0,0 +1,24 @@
+import type { NextFunction, Request, Response } from 'express';
+import { billingService, type V1AdProvider, type V1Entitlement } from './billing.service.js';
+
+export function requireBillingEntitlement(entitlement: V1Entitlement) {
+  return async (req: Request, _res: Response, next: NextFunction) => {
+    try {
+      await billingService.requireEntitlement(req.context.storeId!, entitlement);
+      next();
+    } catch (error) {
+      next(error);
+    }
+  };
+}
+
+export function requireAdProviderEntitlement(provider: V1AdProvider) {
+  return async (req: Request, _res: Response, next: NextFunction) => {
+    try {
+      await billingService.requireAdProvider(req.context.storeId!, provider);
+      next();
+    } catch (error) {
+      next(error);
+    }
+  };
+}
