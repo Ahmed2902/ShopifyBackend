9047e5bcb16cb77b8bec40c970ef933ce5aa8f72
Ahmed Abdelrahman <ahmedabdelrahma@gmail.com>
2026-09-15 20:48:50 +0300

    fix: route exact mapping read through direct lookup [skip ci] [skip vercel]

--- src/modules/meta/mapping/meta-mapping.controller.ts
+++ src/modules/meta/mapping/meta-mapping.controller.ts
@@ -6,7 +6,7 @@ import {
   metaMappingCatalogItemParamsSchema,
   metaMappingListQuerySchema,
 } from './meta-mapping.schema.js';
-import { findSelectedMetaAdMapping } from './meta-mapping.lookup.js';
+import { metaAdMappingLookup } from './meta-mapping.lookup.js';
 import { metaMappingService, type MetaMappingService } from './meta-mapping.service.js';
 
 export class MetaMappingController {
@@ -27,9 +27,7 @@ export class MetaMappingController {
 
   ad = async (req: Request, res: Response) => {
     const { adId } = metaMappingAdParamsSchema.parse(req.params);
-    res.status(200).json(
-      await findSelectedMetaAdMapping(this.service, req.context.storeId!, adId),
-    );
+    res.status(200).json(await metaAdMappingLookup.find(req.context.storeId!, adId));
   };
 
   suggestions = async (req: Request, res: Response) => {
