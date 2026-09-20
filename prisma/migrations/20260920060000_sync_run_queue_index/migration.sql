325da35f4af20c3e5fafaa8e6a82eb55598e2cff
Ahmed Abdelrahman <ahmedabdelrahma@gmail.com>
2026-09-20 16:35:06 +0300

    refactor: persist queued sync lease owner [skip ci] [skip vercel]

--- prisma/migrations/20260920060000_sync_run_queue_index/migration.sql
+++ prisma/migrations/20260920060000_sync_run_queue_index/migration.sql
@@ -1,6 +1,7 @@
 ALTER TABLE "SyncRun"
 ADD COLUMN "activeQueueKey" TEXT,
-ADD COLUMN "leaseExpiresAt" TIMESTAMP(3);
+ADD COLUMN "leaseExpiresAt" TIMESTAMP(3),
+ADD COLUMN "leaseToken" TEXT;
 
 CREATE UNIQUE INDEX "SyncRun_activeQueueKey_key"
 ON "SyncRun"("activeQueueKey");
